395d34653d82027bcc26044d48f132be7aafc173
veeui <m15037392885@126.com>
2021-12-22 17:57:49 +0800

    feat: 新增功能函数

--- packages/shared/src/utils.ts
+++ packages/shared/src/utils.ts
@@ -29,3 +29,43 @@ export function extend( to: any, _from: any ) {
     }
     return to;
 }
+
+export function onBFCacheRestore(callback) {
+    window.addEventListener('pageshow', event => {
+        if(event.persisted) {
+            callback(event)
+        }
+    }, true)
+}
+
+export function onBeforeunload(callback) {
+    window.addEventListener('beforeunload', callback, true)
+}
+
+export function onHidden(callback, once) {
+    const onHiddenOrPageHide = (event) => {
+        if(event.type === 'pagehide' || document.visibilityState === 'hidden') {
+            callback(event)
+            if(once) {
+                window.removeEventListener('visibilitychange', onHiddenOrPageHide, true);
+                window.removeEventListener('pagehide', onHiddenOrPageHide, true)
+            }
+        }
+    }
+
+    window.addEventListener('visibilitychange', onHiddenOrPageHide, true);
+    window.addEventListener('pagehide', onHiddenOrPageHide, true);
+}
+
+export function executeAfterLoad(callback) {
+    if( document.readyState === 'complete' ) {
+        callback()
+    } else {
+        const onLoad = () => {
+            callback();
+            window.removeEventListener('load', onLoad, true)
+        }
+
+        window.addEventListener('load', onLoad, true)
+    }
+}
